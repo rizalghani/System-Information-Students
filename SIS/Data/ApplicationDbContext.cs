47413294946e316084cca0666d46a34c692583fe
Mohammad Rizal Ghani <rizal.pnd.work@gmail.com>
2019-09-08 16:03:15 +0700

    update PPG dan Import

---
+++ SIS/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SIS.Models;
+
+namespace SIS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PPG> PPG { get; set; }
+    }
+}
---
+++ SIS/Models/PPG.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIS.Models
+{
+    public class PPG
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Nopes PPG")]
+        [Required(ErrorMessage = "{0} tidak boleh kosong !")]
+        [StringLength(100, ErrorMessage = "{0} tidak boleh lebih dari {1} angka !")]
+        public string No_PPG { get; set; }
+
+        [Display(Name = "No Ujian")]
+        [Required(ErrorMessage = "{0} tidak boleh kosong !")]
+        [StringLength(100, ErrorMessage = "{0} tidak boleh lebih dari {1} angka !")]
+        public string No_Ujian { get; set; }
+
+        [Display(Name = "Nama")]
+        [Required(ErrorMessage = "{0} tidak boleh kosong !")]
+        [StringLength(100, ErrorMessage = "{0} tidak boleh lebih dari {1} karakter !")]
+        public string Nama { get; set; }
+
+
+        [Display(Name = "Lokasi PPG")]
+        [StringLength(100, ErrorMessage = "{0} tidak boleh lebih dari {1} karakter !")]
+        public string Lokasi { get; set; }
+
+
+        [Display(Name = "Mapel")]
+        [StringLength(100, ErrorMessage = "{0} tidak boleh lebih dari {1} karakter !")]
+        public string Mapel { get; set; }
+
+        [Display(Name = "Stat Gabungan")]
+        [StringLength(50, ErrorMessage = "{0} tidak boleh lebih dari {1} karakter !")]
+        public string Status { get; set; }
+    }
+}
